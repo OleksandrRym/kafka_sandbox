b050cbbf0feeb7df5f0606a5053e7b9bbefca61a
OleksandrRym <lostmannight@gmail.com>
2025-01-06 11:42:27 +0200

    init commit

---
+++ src/main/java/io/conduktor/kfk/KfkApplication.java
@@ -0,0 +1,13 @@
+package io.conduktor.kfk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KfkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KfkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/conduktor/kfk/KfkApplicationTests.java
@@ -0,0 +1,13 @@
+package io.conduktor.kfk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KfkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
