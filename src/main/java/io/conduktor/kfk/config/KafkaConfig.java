d202e65f1fb2df3cdad9b07d53d6c8d764b43d2a
OleksandrRym <lostmannight@gmail.com>
2025-01-07 15:55:31 +0200

    correct kafka config

---
+++ src/main/java/io/conduktor/kfk/config/KafkaConfig.java
@@ -0,0 +1,21 @@
+package io.conduktor.kfk.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+import java.util.Map;
+
+@Configuration
+public class KafkaConfig {
+
+    @Bean
+    NewTopic CreateTopic(){
+        return TopicBuilder.name("product-created-events-topic")
+                .partitions(3)
+                .replicas(3)//1-lead 2-followers
+                .configs(Map.of("min.insync.replicas","2"))
+                .build();
+    }
+}
